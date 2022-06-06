13b9ab14723de1025c384febcc04df5e4c7aab29
howardyeunghk <howardyeunghk@gmail.com>
2022-06-07 00:32:28 +0800

    complie from new version of ts.

--- Chapter01/calculator/calculator-safer.js
+++ Chapter01/calculator/calculator-safer.js
@@ -1,9 +1,9 @@
 function multiply(a, b) {
     var result = a * b;
-    console.log("The multiplication of " + a + "*" + b + " equals to " + result);
+    console.log("The multiplication of ".concat(a, "*").concat(b, " equals to ").concat(result));
     return result;
 }
 multiply(1, 2);
 multiply(2, 2);
 multiply(-10, 10);
-multiply("foo", 'bar');
+//multiply("foo", 'bar');
